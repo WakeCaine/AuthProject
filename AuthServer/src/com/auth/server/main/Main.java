48ebae526c58fde1f3ed2eb4583c7ab9e2729bad
Darius "LittleDev <graffikart@o2.pl>
2015-10-24 23:43:32 +0200

    AuthServer

---
+++ AuthServer/src/com/auth/server/main/Main.java
@@ -0,0 +1,55 @@
+package com.auth.server.main;
+	
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+
+public class Main extends Application{
+	public AnchorPane rootLayout;
+	public static Stage primStage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("MainWindow.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+            MainController controller = (MainController) loader.getController();
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            primStage = primaryStage;
+            
+            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+                @Override
+                public void handle(WindowEvent event) {
+                    Platform.runLater(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            System.out.println("Application Closed by click to Close Button(X)");
+                            controller.closeThread();
+                            System.exit(0);
+                        }
+                    });
+                }
+            });
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
